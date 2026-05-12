424c51d254c95ce5d2161500a2db877ea6a6b8ca
Tom Prasad <gmoto0550@gmail.com>
2026-05-12 16:39:33 -0400

    Add API automation layer with ReqRes integration

--- api/clients/user_client.py
+++ api/clients/user_client.py
@@ -1,2 +1,31 @@
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
 class UserClient:
-    pass
+
+    BASE_URL = "https://reqres.in/api"
+    HEADERS = {
+        "x-api-key": os.getenv("REQRES_API_KEY")
+    }
+
+    def get_user(self, user_id: int):
+        response = requests.get(
+            f"{self.BASE_URL}/users/{user_id}",
+            headers=self.HEADERS
+        )
+        return response
+
+    def create_user(self, name: str, job: str):
+        response = requests.post(
+            f"{self.BASE_URL}/users",
+            json={
+                "name": name,
+                "job": job
+            },
+            headers=self.HEADERS
+        )
+        return response
---
+++ api/tests/test_users_api.py
@@ -0,0 +1,31 @@
+from api.clients.user_client import UserClient
+
+
+def test_get_user():
+
+    client = UserClient()
+
+    response = client.get_user(2)
+
+    response_body = response.json()
+
+    assert response.status_code == 200
+    assert response_body["data"]["id"] == 2
+    assert response_body["data"]["email"] is not None
+
+
+def test_create_user():
+
+    client = UserClient()
+
+    response = client.create_user(
+        name="Tom",
+        job="QA Engineer"
+    )
+
+    response_body = response.json()
+
+    assert response.status_code == 201
+    assert response_body["name"] == "Tom"
+    assert response_body["job"] == "QA Engineer"
+    assert "id" in response_body
